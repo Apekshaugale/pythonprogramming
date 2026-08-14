5b61382d9de7255619e6a941f29e1e2c2b6a4907
Apekshaugale <apekshaugale2020@gmail.com>
2026-08-14 08:44:39 +0530

    output of elif

---
+++ elif condition/elifpracticeoutput.py
@@ -0,0 +1,296 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py ==
+Enter the number : 5
+Buzz
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the number : 15
+Fizz Buzz
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the number : 15
+Fizz Buzz
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the number : 27
+Fizz Buzz
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the number : 15
+Fizz Buzz
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the number : 2
+Traceback (most recent call last):
+  File "C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py", line 16, in <module>
+    if len(num)==1:
+TypeError: object of type 'int' has no len()
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the number : 2
+One digit value is "2".
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the number : 55
+Two digit value is "55".
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the number : 255
+Three  digit value is "255".
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the number : 5555
+More than three   digit value is "5555".
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the number : -1
+Two digit value is "-1".
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the number : 1
+"1"=One.
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the number : 5
+"5"=Five.
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the number : 6
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the number : 9
+Please enter a number between 1 and 5.
+ord('a')
+97
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the character :'f'
+Traceback (most recent call last):
+  File "C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py", line 48, in <module>
+    if ord('A')<=char<=ord('Z'):
+TypeError: '<=' not supported between instances of 'int' and 'str'
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the character :f
+Traceback (most recent call last):
+  File "C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py", line 47, in <module>
+    char=eval(input('Enter the character :'))
+  File "<string>", line 1, in <module>
+    __import__('idlelib.run').run.main(True)
+NameError: name 'f' is not defined
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the character :f
+Traceback (most recent call last):
+  File "C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py", line 47, in <module>
+    char=eval(input('Enter the character :'))
+  File "<string>", line 1, in <module>
+    __import__('idlelib.run').run.main(True)
+NameError: name 'f' is not defined
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the character :'h'
+The given lowercase character is  "h" into  uppercase is "h".
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the character :'h'
+The given lowercase character is  "h" into  uppercase is "H".
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the character :'@'
+The given character is  "@" is special character .
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the character :'A'
+The given uppercase character is  "A" into  lowercase is "a".
+@
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the character :'C'
+The given uppercase character is  "C" into  lowercase is "c".
+B
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the character :'H'
+The given uppercase character is  "H" into  lowercase is "h".
+previous charcter =  G
+Next charcter =  I
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the character :'&'
+The given character is  "&" is special character .
+previous charcter =  %
+Next charcter =  '
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the character :'^'
+The given character is  "^" is special character .
+previous charcter =  ]
+Next charcter =  _
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the character :'fyg'
+The given lowercase character is  "fyg" into  uppercase is "FYG".
+Traceback (most recent call last):
+  File "C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py", line 74, in <module>
+    print('previous charcter = ',chr(ord(char)-1))
+TypeError: ord() expected a character, but string of length 3 found
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the character :'A'
+The given uppercase character is  "A" into  lowercase is "a".
+previous charcter =  @
+Next charcter =  B
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the passward : 'fyvhc'
+Weak 
+VeryStrong
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the passward : 'chhgg'
+Weak 
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the passward : 'dfghjk'
+Medium
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the passward : 'asdfghj$3#'
+Strong
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the passward : '15236547890sgg$#%'
+VeryStrong
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the username :'Apeksha'
+Enter the password : '123@45'
+Traceback (most recent call last):
+  File "C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py", line 106, in <module>
+    if username==unname and password==pword:
+NameError: name 'unname' is not defined. Did you mean: 'uname'?
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the username :'Apeksha'
+Enter the password : '123@45'
+LOGIN SUCCESSFUL
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the username :'Apeksha'
+Enter the password : '47665'
+Incorrect Password .
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the username :'Snehs'
+Enter the password : 'bkjij'
+User Not Found .
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the number : '5'
+Traceback (most recent call last):
+  File "C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py", line 122, in <module>
+    if num>=0 and num%2==0:
+TypeError: '>=' not supported between instances of 'str' and 'int'
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the number : '5'
+Traceback (most recent call last):
+  File "C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py", line 122, in <module>
+    if num>0 and num%2==0:
+TypeError: '>' not supported between instances of 'str' and 'int'
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the number : 5
+Positive Odd
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the number : 4
+Positive Even
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the number : -1
+Zero
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the number : -2
+Negative Even
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the number : 
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the number : -1
+Negative 
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the number : -2
+Negative Even
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the number : -3
+Negative Odd  
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the menu number : 1
+Menu 1 .Idli    Rs.40 
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the menu number : 1
+Menu 1.Idli    Rs.40 
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the menu number : 2
+Menu 2.Poha   Rs.25 
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the menu number : 3
+Menu 3.Tea    Rs.15
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the menu number : 4
+Menu 4.Cold Coffee   Rs.40 
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the menu number : 8
+Menu 8 is Invaild. 
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the assignments submitted number : 75
+Teacher is Happy     
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the assignments submitted number : 45
+Surprise Test Tomorrow!  
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the assignments submitted number : 
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the assignments submitted number : 59
+Teacher is Angry   
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the Money  RS : 1000
+Enter the Battery  %: 20
+Go to a Café ☕
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the Money  RS : 1000
+Enter the Battery  %: 50
+Watch a Movie 🍿
+>>> 
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the Money  RS : 1000
+Enter the Battery  %: 90
+Go on a Trip 🏖
+>>> 
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the Money  RS : 200
+Enter the Battery  %: 100
+Go to a Café ☕
+>>> 
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifpractice.py
+Enter the Money  RS : 100
+Enter the Battery  %: 10
+Stay Home and Study Python 🐍

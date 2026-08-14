74830ab0e3fd3b52c0155b9b9504f5169dd8704a
Apekshaugale <apekshaugale2020@gmail.com>
2026-08-14 08:42:59 +0530

    elif output

---
+++ elif condition/elifoutput.py
@@ -0,0 +1,64 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifprogram.py ===
+Enter the number : 5
+Friday..
+
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifprogram.py ===
+Enter the number1 : 5
+Enter the number 2 : 6
+Enter the operator: '+'
+11
+
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifprogram.py ===
+Enter the number1 : 6
+Enter the number 2 : 8
+Enter the operator: '//'
+0
+
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifprogram.py ===
+Enter the number1 : 5
+Enter the number 2 : 6
+Enter the number 3 : 8
+The greater number is among '6 ' and '8'  is  8
+
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifprogram.py ===
+Enter the number1 : 5
+Enter the number 2 : 6
+Enter the number 3 : 8
+The greater number is among '5 ' and '6'  is  8
+>>> 
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifprogram.py ===
+Enter the number1 : 9
+Enter the number 2 : 15
+Enter the number 3 : 42
+The greater number  among '9 ' and '15'  is  42
+>>> 
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifprogram.py ===
+Enter the number1 : 56
+Enter the number 2 : 5.3
+Enter the number 3 : 15
+The greater number among '5.3 ' and '15'  is  56
+>>> 
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifprogram.py ===
+Enter the number1 : 5
+Enter the number 2 : 8
+Enter the number 3 : 5
+The  smaller  number  among '5 ' and '8'  is  5
+>>> 
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifprogram.py ===
+Enter the number1 : 21
+Love Marriage 
+>>> 
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifprogram.py ===
+Enter the age : 26
+Arrange Marriage
+>>> 
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifprogram.py ===
+Enter the age : 31
+Your choice
+>>> 
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day25/elifprogram.py ===
+Enter the data .'khk'
+It is sequence data type.

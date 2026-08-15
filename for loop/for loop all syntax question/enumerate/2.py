e76d0af805401fa01fc82156f12c1c001a9f1fd0
Apekshaugale <apekshaugale2020@gmail.com>
2026-08-15 08:41:48 +0530

    Add character and word count examples
    Add new script 'for loop/for loop all syntax question/enumerate/2.py' demonstrating basic for-loop usage: counts characters in a string and counts words using split(). The file also contains an incomplete factorial input stub ('num=eval') that needs implementation.

---
+++ for loop/for loop all syntax question/enumerate/2.py
@@ -0,0 +1,20 @@
+#wap to check how many charcter  are present in snetence
+
+a='hello world sentence'
+count=0
+for i in a:
+    count=count+1
+print(count)
+
+
+#wap to check how many word are present in snetence
+
+a='hello world sentence'
+count=0
+for i in a.split():
+    count=count+1
+print(count)
+
+
+#wap to factorial of user input
+num=eval

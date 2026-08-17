5d7d9f813b1da66d553dfedd364e5c34135ffad8
Apekshaugale <apekshaugale2020@gmail.com>
2026-08-17 15:10:42 +0530

    Add forloop workflow tutorial examples
    Add a new file with numerous commented Python examples demonstrating for-loop usage across collections (string, list, tuple, set), dictionary iteration (keys, values, items), and string checks (isdigit, upper, vowel checks). The file contains step-by-step explanatory notes and sample loops intended as a learning/workflow reference.

---
+++ for loop/for loop all syntax question/for loop/forloopoutput.py
@@ -0,0 +1,267 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/looping/for loop/forloop.py
+M   
+o   
+r   
+i   
+n   
+i   
+n   
+g   
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/looping/for loop/forloop.py
+M  o  r  i  n  i  n  g  
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/looping/for loop/forloop.py
+M  o  r  i  n  i  n  g  
+Traceback (most recent call last):
+  File "C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/looping/for loop/forloop.py", line 67, in <module>
+    print(d[items])
+NameError: name 'items' is not defined. Did you mean: 'iter'?
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/looping/for loop/forloop.py
+M  o  r  i  n  i  n  g  
+Traceback (most recent call last):
+  File "C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/looping/for loop/forloop.py", line 67, in <module>
+    print(i,'---->',e[i])
+NameError: name 'e' is not defined
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/looping/for loop/forloop.py
+M  o  r  i  n  i  n  g  
+12 ----> 90
+100 ----> 200
+54 ----> abc
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/looping/for loop/forloop.py
+M  o  r  i  n  i  n  g  
+12 ----> 90
+90
+100 ----> 200
+200
+54 ----> abc
+abc
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/looping/for loop/forloop.py
+M  o  r  i  n  i  n  g  
+12 ----> 90
+90
+<built-in method items of dict object at 0x0000020FCA3E7FC0>
+100 ----> 200
+200
+<built-in method items of dict object at 0x0000020FCA3E7FC0>
+54 ----> abc
+abc
+<built-in method items of dict object at 0x0000020FCA3E7FC0>
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/looping/for loop/forloop.py
+M  o  r  i  n  i  n  g  
+dict_items([(12, 90), (100, 200), (54, 'abc')])
+dict_items([(12, 90), (100, 200), (54, 'abc')])
+dict_items([(12, 90), (100, 200), (54, 'abc')])
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/looping/for loop/forloop.py
+M  o  r  i  n  i  n  g  
+Traceback (most recent call last):
+  File "C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/looping/for loop/forloop.py", line 74, in <module>
+    print(d.i)
+AttributeError: 'dict' object has no attribute 'i'
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/looping/for loop/forloop.py
+M  o  r  i  n  i  n  g  
+(12, 90)
+(100, 200)
+(54, 'abc')
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/looping/for loop/forloop.py
+M  o  r  i  n  i  n  g  
+(12, 90)
+(100, 200)
+(54, 'abc')
+90
+200
+abc
+12
+100
+54
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/looping/for loop/forloop.py
+M  o  r  i  n  i  n  g  
+2
+4
+6
+8
+10
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/looping/for loop/forloop.py
+M  o  r  i  n  i  n  g  
+2
+4
+6
+8
+10
+ABC
+
+PYHTON
+
+JAVA
+
+SQL
+
+XYZ
+
+abc a c
+pyhton p n
+java j a
+sql s l
+xyz x z
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/looping/for loop/forloop.py
+M  o  r  i  n  i  n  g  
+2
+4
+6
+8
+10
+ABC
+
+PYHTON
+
+JAVA
+
+SQL
+
+XYZ
+
+abc a c
+pyhton p n
+java j a
+sql s l
+xyz x z
+2
+3
+1
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/looping/for loop/forloop.py
+M  o  r  i  n  i  n  g  
+2
+4
+6
+8
+10
+ABC
+
+PYHTON
+
+JAVA
+
+SQL
+
+XYZ
+
+abc a c
+pyhton p n
+java j a
+sql s l
+xyz x z
+2 3 1 
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/looping/for loop/forloop.py
+M  o  r  i  n  i  n  g  
+2
+4
+6
+8
+10
+ABC
+
+PYHTON
+
+JAVA
+
+SQL
+
+XYZ
+
+abc a c
+pyhton p n
+java j a
+sql s l
+xyz x z
+2 3 1 G
+d
+ 
+L
+c
+k
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/looping/for loop/forloop.py
+M  o  r  i  n  i  n  g  
+2
+4
+6
+8
+10
+ABC
+
+PYHTON
+
+JAVA
+
+SQL
+
+XYZ
+
+abc a c
+pyhton p n
+java j a
+sql s l
+xyz x z
+2 
+3 
+1 
+G
+
+d
+
+ 
+
+L
+
+c
+
+k
+
+>>> 
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/looping/for loop/forloop.py
+M  o  r  i  n  i  n  g  
+2
+4
+6
+8
+10
+ABC
+
+PYHTON
+
+JAVA
+
+SQL
+
+XYZ
+
+abc a c
+pyhton p n
+java j a
+sql s l
+xyz x z
+2 
+3 
+1 
+G
+d
+ 
+L
+c
+k

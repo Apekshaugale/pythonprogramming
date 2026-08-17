b95b58ca71985c6b7b640823c7be8c3dbdefae21
Apekshaugale <apekshaugale2020@gmail.com>
2026-08-17 15:15:04 +0530

    Add for-loop practice examples
    Add a new practice file with several for-loop exercises and example solutions. The file includes printing each character of a string, iterating lists of numbers, filtering odd numbers, extracting vowels, printing first/last characters of words, and selecting words longer than five characters. Examples are provided as commented blocks for learning and practice.

---
+++ for loop/for loop all syntax question/for loop/practice forloop.py
@@ -0,0 +1,58 @@
+'''
+Print each character of the string.'''
+'''
+s = "Computer"
+for a in s:
+    print(a)
+
+
+s = "Computer"
+for a in s:
+    print(a,end=' ')
+
+    '''
+'''2.
+
+Print all numbers from the list
+l = [10, 20, 30, 40, 50]
+for a in l :
+    print(a,end=' ')
+
+    
+'''
+
+'''Print only odd numbers.
+l = [12, 15, 18, 21, 25, 30]
+for a in l:
+    if a%2==1:
+        print(a)
+'''
+
+'''Print all vowels from the string.
+
+s = "Education"
+for a in s:
+    if a in 'aeiouAEIOU':
+        print(a)
+
+        '''
+'''Print the first character of every word. and Print the last character of every word.
+l = ["apple", "banana", "mango", "orange"]
+for s in l:
+    print(s[0])
+    print(s[-1])
+'''
+
+'''Print words whose length is greater than 5.
+l = ["python", "java", "sql", "javascript", "css"]
+for a in l:
+    if len(a)>5:
+        print(a)
+        '''
+
+'''Print only digits.'''
+
+
+
+  
+

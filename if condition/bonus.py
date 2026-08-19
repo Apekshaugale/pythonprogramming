b5a522cfc3ded4b3a9024726cdb2db596cbb2541
Apekshaugale <apekshaugale2020@gmail.com>
2026-08-19 15:08:45 +0530

    Create bonus.py

---
+++ if condition/bonus.py
@@ -0,0 +1,6 @@
+#86.Employees with more than 5 years of experience receive a bonus
+#experience=6
+exp=eval(input('Enter the Experience : '))
+if exp>5:
+    print(f'The employee has "{exp}" years of experience can receive a bonus.')
+

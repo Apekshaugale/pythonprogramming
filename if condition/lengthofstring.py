b07200e1daafd176472c85eb24eadbdaaf237ff7
Apekshaugale <apekshaugale2020@gmail.com>
2026-08-20 09:19:06 +0530

    Add even-length string check script
    Adds a Python script that checks whether a given string has an even number of characters. It includes a sample string example and a user input prompt to validate the condition.

---
+++ if condition/lengthofstring.py
@@ -0,0 +1,9 @@
+#50.wap to check if the given string has even length of character
+s="hey guys you all are Osam"
+if len(s)%2==0:
+    print('The string has even length of character')
+
+s=eval(input('Enter the string '))
+if len(s)%2==0:
+    print('The string has even length of character')
+

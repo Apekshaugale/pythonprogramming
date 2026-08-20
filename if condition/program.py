deb3f03987961a5acc29d28ee03d7689c03d0387
Apekshaugale <apekshaugale2020@gmail.com>
2026-08-20 09:23:11 +0530

    Add if condition demo programs
    Add a collection of example Python programs demonstrating if conditions. Includes multiple approaches to check if numbers are positive, negative, or even/odd using modulo, integer division, and bitwise operations.

---
+++ if condition/program.py
@@ -0,0 +1,71 @@
+#write a program to check the give number is positive by using user input
+
+'''
+num = eval(input("enter the number :"))
+if num>0:
+    print(f"The give number {num } is positive")
+    '''
+
+#WAP to check given number is -ve
+'''
+
+
+num = eval(input("enter the number :"))
+if num<0:
+    print(f"The give number {num } is negative")
+    
+'''
+
+#wap to check the given number is even
+'''
+a=6
+if a%2==0:
+    print(f"The give number {a} is even")
+'''
+
+#wap to check given number is even wihout using % use( // )
+a=10
+if(a//2)*2==a:
+    print(f"The given number {a} is even.")
+
+
+#wap to check given number is evn without using %,// then which  symbol ---??(use bitwise)
+b=20
+if (b&1)==0:
+    print(f"The given number{b} is even.")
+
+c=23
+if (c&1)==1:
+    print(f'the given number{c} is odd')
+    
+
+
+#for odd use ==1 or for even used ==0
+
+d=23
+if d%2==1:
+    print(f'the given number{d} is odd')
+
+d=23
+if d%2!=0:
+    print(f'the given number{d} is odd')
+
+
+
+    #wap to check word length is even
+a="python"
+if len(a)%2==0:
+    print(f'the given number{d} is even'
+
+
+d="shreya"
+b=len(d)          
+if b%2==0:
+    print(f'the given number{d} is even'
+
+
+
+
+#wap to check given number is even then convert into complex
+          
+    

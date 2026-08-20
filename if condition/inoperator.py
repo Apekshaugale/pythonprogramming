d8210258f2c99c040f8e4ec1a0c527b20313ed01
Apekshaugale <apekshaugale2020@gmail.com>
2026-08-20 09:17:57 +0530

    Add list membership example using in operator
    Add a new script if condition/inoperator.py that demonstrates checking list membership with the `in` operator. The script defines a list of languages and prints a confirmation when "java" is found in the list.

---
+++ if condition/inoperator.py
@@ -0,0 +1,6 @@
+#52.wap to check if the given programming is present in the list
+
+p=["java","python","c","c++","RUBy","golang"]
+if "java" in p:
+    print('The given programming is present in the list.')
+

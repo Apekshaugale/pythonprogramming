43e8be3b87a7a165da788b0e9d433772bed9ee19
Apekshaugale <apekshaugale2020@gmail.com>
2026-08-21 15:41:44 +0530

    Allow withdrawal only if the balance is sufficient

---
+++ if condition/withdrawmoney.py
@@ -0,0 +1,7 @@
+
+#78.Allow withdrawal only if the balance is sufficient.
+balance = 10000
+#if b >w:
+withdraw=eval(input('Enter the amount : '))
+if balance>=withdraw:
+    print('You can withdraw money ')

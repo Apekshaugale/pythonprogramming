7eecc810d0f667dc91904465a51de8974bece21c
Apekshaugale <apekshaugale2020@gmail.com>
2026-08-21 15:41:31 +0530

    check if a character is a vowel.

---
+++ if condition/vowels.py
@@ -0,0 +1,4 @@
+#71.  Write a program to check if a character is a vowel. (e.g., a = 'I')
+string=eval(input('Enter the character: '))
+if  string in 'aeiouAEIOU':
+    print(f'"{ string}"=character is a vowel')

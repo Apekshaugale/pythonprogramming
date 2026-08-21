4b540f82929dfca152d40a88cf8b29bba135888e
Apekshaugale <apekshaugale2020@gmail.com>
2026-08-21 15:40:04 +0530

    checking if a string contains a substring

---
+++ if condition/substring.py
@@ -0,0 +1,12 @@
+#65.wap to checking if a string contains a substring
+
+string="hello world"
+if 'world' in string:
+    print(f'The given string "{string}" contains subtraing "world".')
+
+
+string=eval(input('Enter the string : '))
+if 'world' in string:
+    print(f'The given string "{string}" contains subtraing "world".')
+
+    

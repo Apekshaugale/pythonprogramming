6b0ff52a2c84c6902604da54c6e66aabd6a7dd78
Apekshaugale <apekshaugale2020@gmail.com>
2026-08-22 22:02:30 +0530

    Create if_else notes.py

---
+++ if-else condition/if_else notes.py
@@ -0,0 +1,21 @@
+#if-else
+#if the given condition is True it will execute true state block(if block).
+#but if given confition is False it won't show blank space it will execute in False state block(else block).
+
+
+#syntax---->
+#        if condition:
+#             statement(TSB)
+#        else:
+#            statement(FSB)
+
+
+##flow chart.
+
+
+#            |          |----> True ---> | execute True state block. |--->code will be end
+# start--->    condition
+#            |          |----->False ---->| execute False state block.|--->code will be end
+
+
+

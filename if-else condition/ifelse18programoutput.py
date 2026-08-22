1b80ab57562cfb68548f99db1176960d43899562
Apekshaugale <apekshaugale2020@gmail.com>
2026-08-22 22:04:15 +0530

    Create ifelse18programoutput.py

---
+++ if-else condition/ifelse18programoutput.py
@@ -0,0 +1,299 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py
+Enter the number : 56
+The given number "56" is even.
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py
+Enter the number : 323
+The given number "323" is odd.
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py
+Enter the age of male : 22
+Enter the age of female : 12
+The male of age 22 and female of age 12 are  not eligible for wedding .
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py
+Enter the age of male : 22
+Enter the age of female : 34
+The male of age 22 and female of age 34 are eligible for wedding .
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py
+Enter the character : gfyui
+Traceback (most recent call last):
+  File "C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py", line 21, in <module>
+    char=eval(input('Enter the character : '))
+  File "<string>", line 1, in <module>
+    __import__('idlelib.run').run.main(True)
+NameError: name 'gfyui' is not defined
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py
+Enter the character : 'bkhvtyf'
+character in uppercase= False
+
+================ RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py ================
+Enter the character : 'gjgj'
+character in uppercase= GJGJ
+
+================ RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py ================
+Enter the character : 'GUHI'
+char
+
+================ RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py ================
+Enter the character : "UIIGHJK"
+character in uppercase= uiighjk
+
+================ RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py ================
+Enter the number 1 : 5
+Enter the number 2 : 6
+The "6" is greater than "5" .
+
+================ RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py ================
+Enter the number 1 : 15
+Enter the number 2 : 12
+The "15" is greater than "12" .
+
+================ RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py ================
+Enter the number 1 : 12
+Enter the number 2 : 15
+The "15" is greater than "12" .
+
+================ RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py ================
+Enter the number : 12
+The number is even .
+
+================ RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py ================
+Enter the number : 13
+The number "13" is odd 14
+
+================ RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py ================
+Enter the number : 31
+The number "31" is odd and its in even is  32
+
+================ RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py ================
+Enter the number : 31
+The number "31" is odd and its converted into  even is  32
+
+================ RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py ================
+Enter the character : 'tuijrg'
+Tuijrg
+
+================ RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py ================
+Enter the character : "Rjjj jrhk"
+Rjjj jrhk
+
+================ RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py ================
+Enter the character : 'ghkj fjkgh kj'
+Ghkj fjkgh kj
+
+================ RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py ================
+Enter the number : 5
+Traceback (most recent call last):
+  File "C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py", line 73, in <module>
+    print(f'The given number "{num}" is not even and its exponent is ',e^num)
+NameError: name 'e' is not defined
+
+================ RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py ================
+Enter the string : 'vghkjkjdeo'
+The length of string is even. oedjkjkhgv
+
+================ RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py ================
+Enter the string : 'jgjff'
+The length of string is  not even and it is in uppercase = JGJFF
+
+================ RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py ================
+Enter the number : -3
+The number is negative.
+
+================ RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py ================
+Enter the number : 3
+The number is positive.
+
+================ RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py ================
+Enter the number : 'bjf'
+Traceback (most recent call last):
+  File "C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py", line 70, in <module>
+    if num%2==0:
+TypeError: not all arguments converted during string formatting
+
+================ RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py ================
+Enter the number : 54
+The given number "54" is even and its half is  27.0
+
+================ RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py ================
+Enter the number : 53
+The given number "53" is not even and its exponent is  2809
+
+================ RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py ================
+Enter the data : 56
+Traceback (most recent call last):
+  File "C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py", line 93, in <module>
+    if isinstance(data,(str,tuple,list,set(),dict)):
+TypeError: isinstance() arg 2 must be a type, a tuple of types, or a union
+
+================ RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py ================
+Enter the data : 76
+Traceback (most recent call last):
+  File "C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py", line 93, in <module>
+    if isinstance(data,(str,tuple,list,set(),dict)):
+TypeError: isinstance() arg 2 must be a type, a tuple of types, or a union
+
+================ RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py ================
+Enter the data : '55'
+The given data is collection data type.
+
+================ RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py ================
+Enter the data : 35
+The given data is individual data type .
+
+================ RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py ================
+Enter the data : '{hrk}'
+The given data is individual data type .
+
+================ RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py ================
+Enter the data : 
+================ RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py ================
+Enter the data : 'ufgj'
+The given data is individual data type .
+
+================ RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py ================
+Enter the data : 'fk'
+The given data is individual data type .
+
+================ RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py ================
+Enter the data : 'hgkj'
+The given data is collection data type.
+
+================ RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py ================
+Enter the data : '{}'
+The given data is collection data type.
+
+================ RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py ================
+Enter the data : '[]'
+The given data is collection data type.
+
+================ RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py ================
+Enter the String : 'ruigokA'
+The specified character is present in string.
+
+================ RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py ================
+Enter the String : 'hkrgjtla'
+The specified character is not  present in string.
+
+================ RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py ================
+Traceback (most recent call last):
+  File "C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py", line 118, in <module>
+    print(D[a])
+NameError: name 'a' is not defined
+
+================ RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py ================
+apple
+
+================ RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py ================
+None
+
+================ RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py ================
+{'a': '5', 'b': 'ball', 'c': 'cat'}
+
+================ RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py ================
+{'a': '5', 'b': 'ball', 'c': 'cat'}
+
+================ RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py ================
+{'a': 'apple', 'b': 'ball', 'c': 'cat', 'd': '5'}
+
+================ RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py ================
+Enter the number : 6
+6
+
+================ RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py ================
+Enter the number : 6
+-6
+
+================ RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py ================
+Enter the number : 2
+2
+
+================ RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py ================
+Enter the number : 1
+The given number is smaller than 10 and its exponent is  1
+
+================ RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py ================
+Enter the number : 2
+The given number '2' is smaller than 10 and its exponent is  4
+
+================ RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py ================
+Enter the number : 11
+11
+
+================ RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py ================
+Enter the number : 32
+It is even.
+
+================ RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py ================
+Enter the number : 31
+15.5
+1 15
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py
+Enter the number : 13
+1 6
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py
+Enter the number : 13
+reminder 1
+quotient 6
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py
+Enter the number : 12
+It is even.
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py
+Enter the Character : "A"
+Traceback (most recent call last):
+  File "C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py", line 155, in <module>
+    print('char'**2)
+TypeError: unsupported operand type(s) for ** or pow(): 'str' and 'int'
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py
+Enter the Character : 'A'
+Traceback (most recent call last):
+  File "C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py", line 155, in <module>
+    print(char**2)
+TypeError: unsupported operand type(s) for ** or pow(): 'str' and 'int'
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py
+Enter the Character : 
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py
+Enter the Character : 'A'
+Traceback (most recent call last):
+  File "C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py", line 155, in <module>
+    print(char**2)
+TypeError: unsupported operand type(s) for ** or pow(): 'str' and 'int'
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py
+Enter the Character : 'A'
+Traceback (most recent call last):
+  File "C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py", line 155, in <module>
+    print(char**2)
+TypeError: unsupported operand type(s) for ** or pow(): 'str' and 'int'
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py
+Enter the Character : A
+Traceback (most recent call last):
+  File "C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py", line 153, in <module>
+    char=eval(input('Enter the Character : '))
+  File "<string>", line 1, in <module>
+    __import__('idlelib.run').run.main(True)
+NameError: name 'A' is not defined
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py
+Enter the Character : 's'
+Traceback (most recent call last):
+  File "C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py", line 155, in <module>
+    print(char**2)
+TypeError: unsupported operand type(s) for ** or pow(): 'str' and 'int'
+>>> 
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/ifelse80programtask.py
+Enter the Character : 'p'
+pp

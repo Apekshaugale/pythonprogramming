efc9939b01d0573f13af5f54e264b9a458333e19
Apekshaugale <apekshaugale2020@gmail.com>
2026-08-22 23:35:52 +0530

    Create leapyear_ifelse.py

---
+++ if-else condition/leapyear_ifelse.py
@@ -0,0 +1,6 @@
+#wop to check year is leap or not
+y=eval(input("Enter the year : "))
+if y%4==0:
+     print(f'the given "{y}" year is leap year.')
+else:
+    print("The given year is not a leap year .")

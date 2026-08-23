f14e4627ea6926e107f3aac3aa1e5b032a46713d
Apekshaugale <apekshaugale2020@gmail.com>
2026-08-23 14:30:00 +0530

    Add if-else practice output log
    This commit adds a new output log file documenting multiple Python if/else and collection exercises. It captures trial runs, edge cases, and error examples involving strings, lists, indexing, memory addresses, and character/ASCII operations used for learning and debugging.

---
+++ if-else condition/ouput of ifelse.py
@@ -0,0 +1,320 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+====== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py =====
+Enter the value1:'abc'
+Enter the value2:'abc'
+Traceback (most recent call last):
+  File "C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py", line 23, in <module>
+    mid=(value2[0]+value2[-1])//2
+TypeError: unsupported operand type(s) for //: 'str' and 'int'
+
+== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the value1:[1,2,3]
+Enter the value2:[3,4,5]
+4
+
+== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the value1:
+Traceback (most recent call last):
+  File "C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py", line 20, in <module>
+    value1=eval(input('Enter the value1:'))
+KeyboardInterrupt
+
+== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the value1:[1,2,3]
+Enter the value2:[1,2,3]
+2
+id([1,2,3])
+2349257116736
+
+== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the value1:'a'
+Enter the value2:'s'
+Traceback (most recent call last):
+  File "C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py", line 23, in <module>
+    mid=(value2[0]+value2[-1])//2
+TypeError: unsupported operand type(s) for //: 'str' and 'int'
+
+== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the value1:[1,2,3]
+Enter the value2:[1,2,3]
+ The first item  1   and  memory address is 140711659291768
+ last item   3  and memory address is 140711659291832  
+id(value1)
+1655314429888
+id(value2)
+1655314429184
+
+== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the value1:'a'
+Enter the value2:'a'
+a
+
+== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the value1:'python'
+Enter the value2:'python'
+t
+
+== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the value1:'apeksha'
+Enter the value2:'apeksha'
+k
+
+== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the value1:'ryjh'
+Enter the value2:'nkj'
+
+== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the value1:'lock'
+Enter the value2:'lock'
+Traceback (most recent call last):
+  File "C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py", line 36, in <module>
+    print(v2[mid])
+IndexError: string index out of range
+
+== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the value1:'lock'
+Enter the value2:'lock'
+o
+chr(s)
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    chr(s)
+NameError: name 's' is not defined
+ord('a')
+97
+chr(97)
+'a'
+
+== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the string:'ASDFGHJKKLLLIYTT'
+Traceback (most recent call last):
+  File "C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py", line 47, in <module>
+    if  len(s)>10 and (chr(ord(s[0]))+chr(ord(s[-1])))%5:
+TypeError: not all arguments converted during string formatting
+
+== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the string:'ASDFGHJKKLLLIYTT'
+Traceback (most recent call last):
+  File "C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py", line 47, in <module>
+    a=chr(s[0])
+TypeError: 'str' object cannot be interpreted as an integer
+
+== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the string:'ASDFGHJKKLLLIYTT'
+A
+T
+Traceback (most recent call last):
+  File "C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py", line 51, in <module>
+    if  len(s)>10 and (a+b)%5:
+TypeError: not all arguments converted during string formatting
+
+== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the string:'ASDFGHJKKLLLIYTT'
+A
+T
+A
+
+== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the string:'ASDFGHJKKLLLIYTT'
+first character :  65
+middle character : 75
+last character : 84
+
+== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the string:'ASDFGHJKKLLLIYTTfgg'
+first character :  65
+middle character : 76
+last character : 103
+
+== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the string:'sdfg'
+sdfgsdfgsdfg
+
+== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Traceback (most recent call last):
+  File "C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py", line 57, in <module>
+    OR
+NameError: name 'OR' is not defined
+
+== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the string:'sdfg'
+s
+g
+sdfgsdfgsdfg
+
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the string:'ASDFGHJKKLLLIYTT'
+A
+T
+first character :  65
+middle character : 75
+last character : 84
+
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the list:[1,2,'efsda',5,6]
+5
+
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the list:[1,2,3,4]
+4
+
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the list:[1,2,3,4]
+1
+
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the list:[1,2,'efsda',5,6]
+2
+
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the list:['a','d','c']
+1
+
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the list:['a','d','c']
+['a', 'd', 'c']
+
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the list:[1,2,3,4]
+1
+
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the list:[1,2,3,4]
+2
+
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the list:[1,2,3,4,7]
+3
+
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the number: 45
+180
+
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the number: 21
+Actual value is  : 21
+
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the value1:'abc'
+Enter the value2:'abc'
+2743270304016
+
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the value1:'a'
+Enter the value2:'b'
+140711659358456
+140711659358456
+
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the value1:3
+Enter the value2:3
+140711659291832
+
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the value1:5
+Enter the value2:6
+memory address of value 1 140711659291896
+memory address of value 1 140711659291896
+
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the value1:'rggdsad'
+Enter the value2:'gfds'
+memory address of value 1 2059043859328
+memory address of value 2 2059043859328
+Enter the character:'ghfd'
+ghfdghfdghfdghfdghfd
+
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the character:'grsdc$'
+grsdc$grsdc$grsdc$
+
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the string 1:[1,2,3]
+Enter the string 2:[34,56,66]
+[1, 2, 3, 34, 56, 66]
+
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the string 1:5
+Enter the string 2:[5,6]
+Traceback (most recent call last):
+  File "C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py", line 134, in <module>
+    if len(string1)==len(string2):
+TypeError: object of type 'int' has no len()
+
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the string 1:[6,7]
+Enter the string 2:[6,6,7]
+[6, 7]
+[6, 6, 7]
+
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the variable1:
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the variable1:a
+Traceback (most recent call last):
+  File "C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py", line 143, in <module>
+    s1=eval(input('Enter the variable1:'))
+  File "<string>", line 1, in <module>
+    __import__('idlelib.run').run.main(True)
+NameError: name 'a' is not defined
+
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the variable1:1
+Enter the variable 2:1
+Traceback (most recent call last):
+  File "C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py", line 146, in <module>
+    print(f'The last item of the second collection is "{s2[-1]}".')
+TypeError: 'int' object is not subscriptable
+
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the variable1:'a'
+Enter the variable 2:'a'
+The last item of the second collection is "a".
+
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the variable1:'safgdgfh'
+Enter the variable 2:'safgdgfh'
+The last item of the second collection is "h".
+
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the variable1:'fdygh'
+Enter the variable 2:'cgfg'
+first item of the first collection is f and memory address is  140711659358696
+
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the variable1:[3,5,8]
+Enter the variable 2:[3,5,8]
+first item of the first collection is 3 and memory address is  140711659291832
+
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the string :'safgdgfh'
+safgdgfhsafgdgfhsafgdgfh
+
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the string :"Beast"
+`
+
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the string :
+Traceback (most recent call last):
+  File "C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py", line 159, in <module>
+    string=eval(input('Enter the string :'))
+KeyboardInterrupt
+>>> 
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the string :
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the string :"heart"
+`
+>>> 
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the string :"heart"
+`
+heartheartheartheartheart
+>>> 
+=== RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day23/ifelse.py ==
+Enter the string :'art'
+artartart

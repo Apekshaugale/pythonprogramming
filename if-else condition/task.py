5b7f368468770be19101f4b2476f3fb44b7708c8
Apekshaugale <apekshaugale2020@gmail.com>
2026-08-23 14:33:17 +0530

    Add if-else condition examples
    Adds a Python practice file covering basic if-else conditions, including even/odd checks, uppercase/digit detection, and case conversion examples. The exercises demonstrate comparisons, built-in methods, and manual character checks using ASCII ranges.

---
+++ if-else condition/task.py
@@ -0,0 +1,43 @@
+'''#1.wap to check the given number is even or odd (take user input)
+()
+#
+num=eval(input('Enter the number : '))
+'''
+
+'''
+#wap to check the given number is upper without using inbuild function.
+a='HELLO'
+x='H'
+if ord('A')<=ord(x)<=ord('Z'):
+    print(' The string is uppercase.')
+
+
+#wap to check the given number is digit without using inbuild function.
+
+d='7'
+if ord('0')<=ord(d)<=ord('9'):
+    print("It's a digit.")
+
+#
+e='5'
+if e.isdigit():
+    print('It is a digit.')
+
+
+
+
+#wap to check given cahracter ia upper convert to lower.
+k='HELLO'
+if k.isupper():
+    k=k.lower()
+    print(k)
+
+'''
+#
+d='good morning'
+if d.islower():
+    d=d.upper()
+    print(d)
+
+
+#without using inbuilt fuction upper into lower and vicevarsa 

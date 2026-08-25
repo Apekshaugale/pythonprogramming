a91b77ffc874e32bec380f0e3c02a80a6b2c030f
Apekshaugale <apekshaugale2020@gmail.com>
2026-08-25 09:14:20 +0530

    Add complex number tutorial notes
    Adds a beginner-friendly Python note file covering complex numbers, how to inspect their type, use bool() on complex values, and access the real/imaginary parts with attributes. This is a learning example in the Intro to python section.

---
+++ Intro to python/complex data type.py
@@ -0,0 +1,17 @@
+'''
+
+we can't reverse the syntax of complex data type .
+type()--->to find datatype
+bool()--->to check default value
+
+
+#to fetch real part =var.real
+#to fetch imaginary part= var.imaginary
+
+'''
+'''
+a=3+5j
+bool(3+5j)
+'''
+a=2+3j
+a.imag

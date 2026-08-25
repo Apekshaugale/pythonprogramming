7dc99f57a1adba9c626ee12776654c22e46916ba
Apekshaugale <apekshaugale2020@gmail.com>
2026-08-25 09:13:16 +0530

    Add boolean data type notes
    Add 'Intro to python/boolean data type.py' containing a raw Python REPL transcript and notes about booleans and complex numbers. Includes examples accessing .real/.imag, commentary on bool(), True/False, and a recorded SyntaxError from an unterminated string.

---
+++ Intro to python/boolean data type.py
@@ -0,0 +1,44 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+= RESTART: C:/Users/Apeksha/Desktop/PythonProgramming/pythonprogramming/Intro to python/complex data type.py
+a=2+3j
+a.imag
+3.0
+>>> a.real
+2.0
+>>> 
+>>> 
+>>> 
+... we can't reverse the syntax of complex data type .
+... type()--->to find datatype
+... bool()--->to check default value
+... 
+... 
+... #to fetch real part =var.real
+... #to fetch imaginary part= var.imaginary
+... 
+... '''
+... '''
+... a=3+5j
+... bool(3+5j)
+... '''
+SyntaxError: unterminated string literal (detected at line 2)
+>>> 
+>>> #boolean datatype
+>>> #it is a individiual data type which consists of only 2 values in it .[True ,False]
+>>> #bool function will not consists of True and False.
+>>> #bool data type contains True and False
+>>> 
+>>> #True and False are special keyword
+>>> 
+>>> #here True   is non-default value
+>>> 
+>>> #False =Default value
+>>> 
+>>> #    bool()                                       boolean datatype
+>>> # it is a inbuild function                 it is a individual datatype
+>>> #it is display result                          it will contain values like True and False
+>>> #in form of True and False
+>>> 
+>>> 

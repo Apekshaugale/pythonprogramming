12c8d554dde236fc52b56e15a376801581889845
Apekshaugale <apekshaugale2020@gmail.com>
2026-08-28 00:41:07 +0530

    Create grater than 4.py

---
+++ nestedif/grater than 4.py
@@ -0,0 +1,15 @@
+a=input('Enter the number :')
+b=input('Enter the number :')
+c=input('Enter the number :')
+d=input('Enter the number :')
+if a>b:
+    if a>c:
+        if a>d:
+            print(a,' is grater.')
+        else:
+            print(d,' is greater')
+    else:
+        if c>d:
+           if b>d:
+               print(b,' is greater')
+               

4604f952ed5537c810b48d031047b1f4617bc255
Apekshaugale <apekshaugale2020@gmail.com>
2026-08-28 00:41:45 +0530

    Create nestedifoutput.py

---
+++ nestedif/nestedifoutput.py
@@ -0,0 +1,21 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/nested if program.py
+Enter the list : [2,4,'level']
+level is a palindrome ans start with consonant .
+>>> 
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/nested if program.py
+Enter the username : 'Apeksha'
+Enter the password234
+Invaild Password.
+>>> 
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/nested if program.py
+Enter the username : 'priya'
+Enter the password123%35
+User not found
+>>> 
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/nested if program.py
+Enter the username : 'Apeksha'
+Enter the password123%35
+Login Successful...

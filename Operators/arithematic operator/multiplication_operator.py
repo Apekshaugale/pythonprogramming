c27c66408c15f4c31957c824afec656139c5c386
Apekshaugale <apekshaugale2020@gmail.com>
2026-08-29 10:37:33 +0530

    multiplication operator

---
+++ Operators/arithematic operator/multiplication_operator.py
@@ -0,0 +1,63 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #syntax-->op1*n
+>>> #where n is integier
+>>> 
+>>> 1*4
+4
+>>> 
+>>> (5j)*(4+5j)
+(-25+20j)
+>>> 
+>>> 
+>>> 5.6*9.6
+53.76
+>>> 
+>>> 
+>>> True*False
+0
+>>> 
+>>> False*True
+0
+>>> 
+>>> 5*(4+5j)
+(20+25j)
+>>> 
+>>> 5*(4+5j)
+(20+25j)
+>>> 
+>>> False*(4+5j)
+0j
+>>> 
+False*5
+0
+
+5.6*(6j)
+33.599999999999994j
+
+'ghyyj'*4
+'ghyyjghyyjghyyjghyyj'
+
+'ghyyj'*(5.7)
+Traceback (most recent call last):
+  File "<pyshell#27>", line 1, in <module>
+    'ghyyj'*(5.7)
+TypeError: can't multiply sequence by non-int of type 'float'
+
+[3,5,6]*9
+[3, 5, 6, 3, 5, 6, 3, 5, 6, 3, 5, 6, 3, 5, 6, 3, 5, 6, 3, 5, 6, 3, 5, 6, 3, 5, 6]
+
+{67,89}*2
+Traceback (most recent call last):
+  File "<pyshell#31>", line 1, in <module>
+    {67,89}*2
+TypeError: unsupported operand type(s) for *: 'set' and 'int'
+
+{5:7}*4
+Traceback (most recent call last):
+  File "<pyshell#33>", line 1, in <module>
+    {5:7}*4
+TypeError: unsupported operand type(s) for *: 'dict' and 'int'
+
+#set will not allow duplicate values.and dictionary visible layer is only key layer so we can't do multiplication
+

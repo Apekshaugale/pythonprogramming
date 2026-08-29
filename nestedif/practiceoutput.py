512f3fee5f97a7d13a2edac6210edd6379d4d6d1
Apekshaugale <apekshaugale2020@gmail.com>
2026-08-29 10:36:14 +0530

    practiceoutput

---
+++ nestedif/practiceoutput.py
@@ -0,0 +1,339 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/passward.py
+Enter the user name :
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/passward.py
+Enter the string'Apeksha'
+The given data type is string
+Enter the option(1,2,3,4)1
+APEKSHA
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/passward.py
+Enter the string'Apeksha'
+The given data type is string
+Enter the option(1,2,3,4)2
+apeksha
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/passward.py
+Enter the string'Apeksha'
+The given data type is string
+Enter the option(1,2,3,4)3
+aPEKSHA
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/passward.py
+Enter the string'Apeksha'
+The given data type is string
+Enter the option(1,2,3,4)4
+Traceback (most recent call last):
+  File "C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/passward.py", line 33, in <module>
+    print(string.capitialize())
+AttributeError: 'str' object has no attribute 'capitialize'. Did you mean: 'capitalize'?
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/passward.py
+Enter the string'Apeksha'
+The given data type is string
+Enter the option(1,2,3,4)4
+Traceback (most recent call last):
+  File "C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/passward.py", line 33, in <module>
+    print(string.capitiaize())
+AttributeError: 'str' object has no attribute 'capitiaize'. Did you mean: 'capitalize'?
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/passward.py
+Enter the string'Apeksha'
+The given data type is string
+Enter the option(1,2,3,4)4
+Traceback (most recent call last):
+  File "C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/passward.py", line 33, in <module>
+    print(string.capitaize())
+AttributeError: 'str' object has no attribute 'capitaize'. Did you mean: 'capitalize'?
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/passward.py
+Enter the string'Apeksha'
+The given data type is string
+Enter the option(1,2,3,4)4
+Apeksha
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/passward.py
+Enter the string'Apeksha'
+The given data type is string
+Enter the option(1,2,3,4)5'
+Traceback (most recent call last):
+  File "C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/passward.py", line 23, in <module>
+    option=eval(input('Enter the option(1,2,3,4)'))
+  File "<string>", line 1
+    5'
+     ^
+SyntaxError: unterminated string literal (detected at line 1)
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/passward.py
+Enter the string'Apeksha'
+The given data type is string
+Enter the option(1,2,3,4)5
+Enter the valid option
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/passward.py
+Enter the theater name : 'Miraj'
+User is selected the  "{theater}".
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/passward.py
+Enter the theater name : 'Miraj'
+User is selected the  "['Miraj', 'PVR', 'Lakxmi', 'Cinipole']".
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/passward.py
+Enter the theater name : 'Miraj'
+User is selected the  "Miraj".
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/passward.py
+Enter the theater name : "MIraj"
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/passward.py
+Enter the theater name : 
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/passward.py
+Given data type is list .
+The given middle element is odd. 
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/passward.py
+Given data type is list .
+7
+The given middle element is odd. 
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/passward.py
+Enter the app name : flipkart
+Traceback (most recent call last):
+  File "C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/passward.py", line 94, in <module>
+    user=eval(input('Enter the app name : '))
+  File "<string>", line 1, in <module>
+    __import__('idlelib.run').run.main(True)
+NameError: name 'flipkart' is not defined
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/passward.py
+Enter the app name : 'flipkart'
+User selected the app name is flipkart
+Categories : 
+1.electronics
+2.mobile
+3 .fashion
+4 .furnitures
+Enter the categories : 
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/passward.py
+Enter the app name : 'amazon'
+User selected the app name is amazon
+Select  the categories : 2
+Categories : 
+1.electronics
+2.mobile
+3 .fashion
+4 .furnitures
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/passward.py
+Enter the app name : 'amazon'
+User selected the app name is amazon
+Categories : 
+1.electronics
+2.mobile
+3 .fashion
+4 .furnitures
+Select the categories : 
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/passward.py
+Enter the app name : 'amazon'
+User selected the app name is amazon
+Categories : 
+1.electronics
+2.mobile
+3 .fashion
+4 .furnitures
+Select the categories : electronics
+Traceback (most recent call last):
+  File "C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/passward.py", line 103, in <module>
+    c_gories=eval(input('Select the categories : '))
+  File "<string>", line 1, in <module>
+    __import__('idlelib.run').run.main(True)
+NameError: name 'electronics' is not defined
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/passward.py
+Enter the app name : 'amazon'
+User selected the app name is amazon
+Categories : 
+1.electronics
+2.mobile
+3 .fashion
+4 .furnitures
+Select the categories : 'electronics'
+User select the category : {c_gories}
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/passward.py
+Enter the app name : 'amazon'
+User selected the app name is amazon
+Categories : 
+1.electronics
+2.mobile
+3 .fashion
+4 .furnitures
+Select the categories : 'electronics'
+User select the category : electronics
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/passward.py
+['flipkart', 'amazon']
+Enter the app name : 
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/passward.py
+Available apps are :  ['flipkart', 'amazon']
+Enter the app name : 
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/passward.py
+Available apps are :  ['flipkart', 'amazon']
+Enter the app name : 'flipkart'
+User selected the app name is flipkart
+Categories : 
+1.electronics
+2.mobile
+3 .fashion
+4 .furnitures
+Select the categories : 'fashion'
+User select the category : fashion 
+ fashion does not contains the mobile select vaild category
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/passward.py
+Available apps are :  ['flipkart', 'amazon']
+Enter the app name : 'amazon'
+User selected the app name is amazon
+Categories : 
+1.electronics
+2.mobile
+3 .fashion
+4 .furnitures
+Select the categories : 'mobile'
+User select the category : mobile 
+Enter the mobile name : 'nothing'
+Added to cart nothing  mobile  .
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/passward.py
+Available apps are :  ['flipkart', 'amazon']
+Enter the app name : 'amazon'
+User selected the app name is amazon
+Categories : 
+1.electronics
+2.mobile
+3 .fashion
+4 .furnitures
+Select the categories : 5
+Invaild category 
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/passward.py
+Available apps are :  ['flipkart', 'amazon']
+Enter the app name : 1
+App is not available
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/passward.py
+Available apps are :  ['flipkart', 'amazon']
+Enter the app name : 'flipkart'
+User selected the app name is flipkart
+Categories : 
+1.electronics
+2.mobile
+3 .fashion
+4 .furnitures
+Select the categories : 1
+Invaild category 
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/passward.py
+Enter the product 1:'lassi'
+Enter the product 2:'ghee'
+Enter the product 3:'dahi'
+Enter the product 4:'tup'
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/passward.py
+Enter the product 1:'lassi'
+Enter the product 2:'ghee'
+Enter the product 3:'dahi'
+Enter the product 4:'tup'
+('lassi', 'ghee', 'dahi', 'tup')
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/passward.py
+Enter the product 1:
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/passward.py
+Enter the product 1:'lassi'
+Enter the price of product 1 Rs:300
+Enter the product 2:'ghee'
+Enter the price of product 2 Rs:590
+Enter the product 3:'dahi'
+Enter the price of product 3 Rs:570
+Enter the product 4:'tup'
+Enter the price of product 4  Rs:700
+Selected product are  :  ('lassi', 'ghee', 'dahi', 'tup')
+Selected product  price are  :  (300, 590, 570, 700)
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/passward.py
+Enter the product 1:'lassi'
+Enter the price of product 1 Rs:500
+Enter the product 2:'ghee'
+Enter the price of product 2 Rs:600
+Enter the product 3:'dahi'
+Enter the price of product 3 Rs:700
+Selected product are  :  ('lassi', 'ghee', 'dahi')
+Selected product  price are  :  (500, 600, 700)
+Enter the payment option :'tup'
+You are not eligible for 10 % off  because you selected different payment mode ..
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/passward.py
+Enter the product 1:'lassi'
+Enter the price of product 1 Rs:500
+Enter the product 2:'lassi'
+Enter the price of product 2 Rs:822
+Enter the product 3:'lassi'
+Enter the price of product 3 Rs:860
+Selected product are  :  ('lassi', 'lassi', 'lassi')
+Selected product  price are  :  (500, 822, 860)
+Enter the payment option :credit card
+You are eligible for 10 % off ...
+You are eligible for 10 % off and you are satified condition first of minimun order 3.
+You are  not eligible  for 10 % off  because you are not satified condition second  .
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/passward.py
+Enter the product 1:
+Traceback (most recent call last):
+  File "C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/passward.py", line 126, in <module>
+    p1=eval(input ('Enter the product 1:'))
+KeyboardInterrupt
+
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/passward.py
+Enter the product 1:'lassi'
+Enter the price of product 1 Rs:500
+Enter the product 2:'lassi'
+Enter the price of product 2 Rs:505
+Enter the product 3:'lassi'
+Enter the price of product 3 Rs:800
+Selected product are  :  ('lassi', 'lassi', 'lassi')
+Selected product  price are  :  (500, 505, 800)
+Enter the payment option :credit card
+You are eligible for 10 % off ...
+You are eligible for 10 % off and you are satified condition first of minimun order 3.
+You are  not eligible  for 10 % off  because you are not satified condition second  .
+>>> 
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/passward.py
+Enter the product 1:'lassi'
+Enter the price of product 1 Rs:820
+Enter the product 2:'lassi'
+Enter the price of product 2 Rs:625
+Enter the product 3:'lassi'
+Enter the price of product 3 Rs:666
+Selected product are  :  ('lassi', 'lassi', 'lassi')
+Selected product  price are  :  (820, 625, 666)
+Enter the payment option :credit card
+You are eligible for 10 % off ...
+You are eligible for 10 % off and you are satified condition first of minimun order 3.
+You are eligible for 10 % off and you are satified condition both condition .
+total_price of product is : 2111
+10% discount  : 211.1
+total_price of product  after 10%  of discount  : 1899.9
+>>> 
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day26/nestedif/passward.py
+Enter the product 1:'lassi'
+Enter the price of product 1 Rs:800
+Enter the product 2:'apple'
+Enter the price of product 2 Rs:852
+Enter the product 3:'papaya'
+Enter the price of product 3 Rs:832
+Selected product are  :  ('lassi', 'apple', 'papaya')
+Selected product  price are  :  (800, 852, 832)
+Enter the payment option :phonepay
+You are not eligible for 10 % off  because you selected different payment mode ..

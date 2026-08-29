54d1f43ce4c50283568ddba5c52bffe1074757b2
Apekshaugale <apekshaugale2020@gmail.com>
2026-08-29 10:38:06 +0530

    operators program

---
+++ Operators/arithematic operator/operatorprogram.py
@@ -0,0 +1,10 @@
+n1=int(input('enter num 1 :'))
+n2=int(input('enter num 2 :'))
+print('addition of two number is :',n1+n2)
+
+print('subtraction of two number is :',n1-n2)
+print('True division of two number is :',n1/n2)
+
+print('Floor division of two number is :',n1//n2)
+print('Modulos division of two number is :',n1%n2)
+//

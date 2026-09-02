f08f2156127f07541aa23d11fb37685c8a4fe963
Apekshaugale <apekshaugale2020@gmail.com>
2026-09-02 20:12:46 +0530

    Add introductory notes on Python functions
    Introduce a new file 'function/function and arguments/intro to function.py' containing commented notes on function concepts: definition, user-defined vs built-in, syntax and calling, and a list of argument types (positional, keyword, *args, **kwargs and combinations).

---
+++ function/function and arguments/intro to function.py
@@ -0,0 +1,44 @@
+#function
+#   set of code or block of code it will execute when we calling the function .
+#   without calling function if we execute it will show blank space .
+
+# complete function depends on syntax without syntax it won't works
+
+# There are two types of function
+# 1.predefied function /inbuild function
+# 2.user defined function
+
+#if we want to avoid repetation , to arrange code in systamatic type
+
+#userdefined syntax :
+#       1.without using return keyword
+#       2.with using return keyword
+
+
+#       1.without using return keyword
+
+
+#syntax :
+#       def dunction-name(parameter):                             ----------function declartion
+#                       statement
+#                       statement                                              ----------block of code
+
+#        funvtion_name(argument)                              -----------function calling
+
+
+
+
+
+# types of the argument
+
+# 1.Positional argument.
+# 2.keyword argument.
+#3. Only Positional argument.
+#4.Only keyword argument.
+#5.variable positional argument(*args)
+#6. variable keyword argumnet(**kwargs)
+#7.combination of only Positional argument   and only keyword arguments
+#8.combination of *args and **kwargs( combination of variable positional argument and variable keyword argumnet)
+
+
+

0e3592edea197e06aae7fb64f34b00d8ec1bf1ec
Apekshaugale <apekshaugale2020@gmail.com>
2026-09-04 13:04:38 +0530

    Add slicing examples for strings
    This commit adds a Python practice file demonstrating string slicing with different start, stop, and step combinations, including negative indexing. It serves as a learning example for how slicing behaves in Python and how to reason about slice boundaries.

---
+++ silcing and typecasting/combinationslic.py
@@ -0,0 +1,23 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> a='pythonprogramming'
+>>> #p y t h o n p r o g r a m m i n g
+>>> #1 2  3 4 5 6  7 8 9 10 11 12 13 14 15
+>>> a[3:-13:1]
+'h'
+>>> a[3:13:1]
+'honprogram'
+>>> a[3:11:1]
+'honprogr'
+>>> a[3:-11:1]
+'hon'
+>>> a[9:-5:-1]
+''
+>>> a[11:-9:-1]
+'arg'
+>>> a[14::1]
+'ing'
+>>> a[:-2:-1]
+'g'
+>>> a[:-4:-1]
+'gni'

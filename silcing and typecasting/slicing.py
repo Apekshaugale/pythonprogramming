9d8a82ddea9e1ec8c670fa845a87eb51ea716850
Apekshaugale <apekshaugale2020@gmail.com>
2026-09-06 20:41:52 +0530

    Add Python slicing examples
    This commit adds a new Python example file that demonstrates basic string slicing, reverse slicing, and step-based indexing. It includes examples for start/stop ranges, default behavior, and reversing strings to illustrate common slicing patterns in Python.

---
+++ silcing and typecasting/slicing.py
@@ -0,0 +1,24 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+= RESTART: C:/Users/Apeksha/AppData/Local/Programs/Python/Python314/py.py
+a='hello pratiksha'
+>>> a[0:4+1:1]
+'hello'
+>>> a[:4+1:]
+'hello'
+>>> a[9:15+1:1]
+'tiksha'
+>>> a[9::]
+'tiksha'
+>>> a[-1::-1]
+'ahskitarp olleh'
+>>> a[::-1]
+'ahskitarp olleh'
+>>> a[::1]
+'hello pratiksha'
+>>> s='hello'
+>>> s[::2]
+'hlo'
+>>> s[1::2]
+'el'

11758509f5beb5ecb487b23697eda3631ccb410f
Apekshaugale <apekshaugale2020@gmail.com>
2026-09-06 20:35:21 +0530

    Add combination slicing REPL example
    Add a new REPL-transcript example file demonstrating string slicing (forward and backward) under silcing and typecasting/combination slicing.py. The file shows attempts on 'morning' and 'Good luck', includes a SyntaxError from a multi-statement compile and final slice results ('orn', 'doo'). (Filename contains a minor typo: 'silcing' instead of 'slicing'.)

---
+++ silcing and typecasting/combination slicing.py
@@ -0,0 +1,23 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/combination slicing.py
+>>> 
+= RESTART: C:/Users/Apeksha/Desktop/webGIT/QSP/pyhton/day22/combination slicing.py
+>>> #combination slicing
+... 
+... #1.forward  slicing
+... #2.Backward slicing
+... 
+... x='morning'
+... x[1:4:1 ]
+SyntaxError: multiple statements found while compiling a single statement
+>>> x='morning'
+>>> x[1:4:1 ]
+'orn'
+>>> y='Good luck'
+>>> y
+'Good luck'
+>>> y[3:-9:-1]
+'doo'
+>>> 

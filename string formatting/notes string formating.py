c6c3a83dd44eee90cd888392646ff1803af5b35f
Apekshaugale <apekshaugale2020@gmail.com>
2026-09-07 09:46:29 +0530

    Add string formatting notes
    This commit adds a Python notes file covering string formatting basics. It explains placeholder formatting, the .format() method, and f-strings, including how different data types are represented and the differences between them.

---
+++ string formatting/notes string formating.py
@@ -0,0 +1,24 @@
+#string formatting
+It is process of inserting value or variables inside a string.
+1.formating with placeholder
+2..format method
+3.formating with f-literals
+
+1.formating with placeholder-->
+we use % symbols
+%s is used for string data
+%f is used for float data
+%d is used for integer data
+
+we can not change data type
+
+2..format method  ---->
+we can chnage the data type
+we have to write in parenthesis
+
+3.formating with f-literals
+
+we have to use {}
+and inside {} we have to put varible name in it
+before quotes we have to use manditory f/F
+

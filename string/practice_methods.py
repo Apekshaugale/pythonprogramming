53df01f280a0411fd4e02f5ba007abd54b804ab5
Apekshaugale <apekshaugale2020@gmail.com>
2026-09-09 09:51:29 +0530

    Add string practice methods examples
    Add string/practice_methods.py containing a Python REPL-style transcript demonstrating common string methods (upper, lower, title, capitalize, swapcase, count). The file is an examples transcript rather than an executable script and documents interactive usage of these methods.

---
+++ string/practice_methods.py
@@ -0,0 +1,36 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+#upper()
+a='python'
+a.upper()
+'PYTHON'
+>>> 
+>>> #lower
+>>> a='HELLO'
+>>> a.lower()
+'hello'
+>>> 
+>>> #title
+>>> a='my name is apeksha'
+>>> a.title()
+'My Name Is Apeksha'
+>>> 
+>>> #capitalize
+>>> a='welcome'
+>>> a.capitalize()
+'Welcome'
+>>> #swapcase
+>>> a='PyThOn'
+>>> a.swapcase()
+'pYtHoN'
+>>> 
+>>> #count()
+>>> a='banana'
+>>> a.count('a',2,5)
+1
+>>> a.count('a',2)
+2
+>>> a.count('a')
+3
+>>> a.count('a',2
+

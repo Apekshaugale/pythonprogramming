77a2b50cbfa72964004071c83a899d6ec906f077
Apekshaugale <apekshaugale2020@gmail.com>
2026-09-10 22:50:25 +0530

    Add inheritance example: Dad/child classes
    Add oops/method/inheritance/1.py demonstrating simple inheritance. Defines Dad with a cash attribute and villa() method and a child subclass with home(); prints dir(Dad) (duplicated) and includes commented example usage showing instantiation and method calls for testing/learning purposes.

---
+++ oops/method/inheritance/1.py
@@ -0,0 +1,14 @@
+class Dad:   
+   cash=100000
+   def villa(self): #parent class method
+       print('Dads villa')
+class child(Dad):
+    def home(self): #child class method
+        bike='BMW'
+        print("Dad's gift")
+print(dir(Dad))
+print(dir(Dad))
+#d=child()
+#d.villa()
+#d.home()
+#print(d.cash)

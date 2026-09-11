17c28a3e99b4b3be0e34f2537ca6db12cdacce28
Apekshaugale <apekshaugale2020@gmail.com>
2026-09-11 22:46:05 +0530

    Add single-level inheritance examples
    Add oops/method/inheritance/single level inheritance/1.py containing multiple example snippets and notes demonstrating single-level inheritance in Python. Includes examples of method overriding, use of super(), constructors, attribute and class variable behavior, and several practice exercises for students. File is primarily educational with commented code and small runnable snippets.

---
+++ oops/method/inheritance/single level inheritance/1.py
@@ -0,0 +1,274 @@
+'''class Dad:   
+   cash=100000
+   def villa(self): #parent class method
+       print('Dads villa')
+class child(Dad):
+    def home(self): #child class method
+        bike='BMW'
+        print("Dad's gift")
+print(dir(Dad))
+print(dir(Dad))
+#d=child()
+#d.villa()
+#d.home()
+#print(d.cash)
+
+'''
+
+'''
+class Dad:
+    cash=100000
+    def villa(self):
+        print("Dad's villa")
+
+class Child(Dad):
+    bike_name="Bmw"
+    def Home(self):
+        self.cash=900
+        print(http://self.cash)
+        print("Dad's Gift")
+
+'''
+
+
+'''
+print(dir(Dad))
+print()
+print(dir(Child))
+'''
+'''
+
+d=Child()
+print(d.cash)
+d.villa()
+d.Home()
+'''
+
+'''
+class Dad:
+    def spam(self):
+        print("spam method")
+
+class Child(Dad):
+    def spam(self):
+        # Dad.spam(self)
+        super().spam()
+        print("child method")
+c=Child()
+c.spam()
+'''
+
+'''
+class A:
+    def student_data(self,name,age):
+        self.name=name
+        self.age=age
+        print(f'student Name is {self.name}\n'
+              f'student age is {self.age}')
+
+class B(A):
+    def student_data(self,sub,usn):
+        self.sub=sub
+        self.usn=usn
+        # super().student_data("XYZ",24)
+        A.student_data(self,"ABC",17)
+        print(f'subject Name is {self.sub}\n'
+              f'student USN number is {self.usn}')
+
+b=B()
+b.student_data("Python","P1234")
+'''
+
+'''
+
+class A:
+    def student_data(self,name,age):
+        self.name=name
+        self.age=age
+        print(f'student Name is {http://self.name}\n'
+              f'student age is {self.age}')
+
+class B(A):
+    def student_data(self,name,age,sub,usn):
+        self.sub=sub
+        self.usn=usn
+        A.student_data(self,name,age)
+        # super().student_data(name,age)
+        print(f'subject Name is {self.sub}\n'
+              f'student USN number is {self.usn}')
+
+b=B()
+b.student_data("AB",24,"Excel","P1234")
+'''
+'''
+class Test:
+    def __init__(self):
+        print("C1")
+
+class Data(Test):
+    def __init__(self):
+        super().__init__()
+        Test.__init__(self)
+        print("C2")
+d=Data()
+'''
+
+
+'''
+class Company:
+    def __init__(self,name,sal,yop):
+        self.name=name
+        self.sal=sal
+        self.yop=yop
+
+    def Data(self):
+        print(f'employee name is {http://self.name}\n'
+              f'total salary is {self.sal}\n'
+              f'yop is {self.yop}')
+
+class Information(Company):
+    def __init__(self,eid,role,add):
+        self.eid=eid
+        self.role=role
+        self.add=add
+        super().__init__("Rahul",45000,5)
+
+    def Data(self):
+        super().Data()
+        print(f'employee id is {self.eid}\n'
+              f'Role is {self.role}\n'
+              f'current address is {self.add}')
+
+i=Information("R13","Analysis","Pune")
+http://i.Data()
+
+'''
+
+#-------------------------------------------example-------------------------------------------------------------#
+'''
+class Dad:
+   def spam(self):
+       print("Dad's class")
+class Child(Dad):
+   def demo(self):
+       print("Child class")
+x=Child()
+x.demo()#Child class
+x.spam()#Dad's class
+Child.spam(x)#Dad's class
+Child.demo(x)#Child class
+
+#-----------method overriding------------------------------------------------------
+#child class has a method with the same name and same parameters:
+class Dad:
+   def spam(self):
+       print("Dad's class")
+class Child(Dad):
+   def spam(self):
+       print("Child class")
+x=Child()
+x.spam()#Child class
+Child.spam(x)#Child class
+
+'''
+
+
+class Dad:
+   money=100
+   def villa(self):
+       print("Dad's villa")
+class Child(Dad):
+   name="Joy"
+   def Study(self):
+       print("No study")
+x=Child()
+print(x.money)#100
+print(x.name)#Joy
+'''x.money=2000
+print(Dad.money)#100
+
+x.money=2000
+print(x.money)#2000
+'''
+Dad.money=2000
+print(x.money)#2000
+
+Dad.money=2000
+print(Dad.money)#2000
+'''
+class Dad:
+   def show(self):
+       print("method-->1")
+class Child(Dad):
+   def show(self):
+       super().show()
+       print("method-->2")
+s=Child()
+s.show()
+
+
+class Parent:
+   def __init__(self):
+       print("constructor class 1")
+
+
+class Child(Parent):
+   def __init__(self):
+       super().__init__()
+       print("constructor class 2")
+
+
+c=Child()
+
+
+
+
+
+
+class Student:
+defstudent_data(self,name,sub):
+       self.name=name
+       self.sub=sub
+       print(f'StudentName is {self.name}')
+       print(f'SUbject Name is {self.sub}')
+
+
+class Information(Student):
+defstudent_data(self,clg_name,marks):
+       super().student_data(name="XYZ",sub="SQL")
+       self.clg=clg_name
+       self.marks=marks
+       print(f'CLG Name is {self.clg}')
+       print(f'Total Marks is {self.marks}')
+
+
+i=Information()
+i.student_data("PYSPIDERS",99)
+
+
+
+
+
+Assignment Questions
+
+1.Create a class Employee with attributes name and salary.
+Create a child class Manager that prints both values.
+2.Create a class Bank with attribute balance.
+ Create a child class SavingsAccount with methods:
+deposit()
+withdraw() 
+
+
+3.Create a class Person with a constructor.
+Create a child class Student and use super() to initialize parent data.
+
+
+
+4.Create a class Vehicle with method start().
+Override this method in child class Bike.
+
+
+'''
+
+
+
